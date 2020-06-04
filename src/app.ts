b8ac218169af6d7c9c463c82db3659bd6dd87347
Jonathan Manera <jonathan.manera@globallogic.com>
2020-06-04 08:37:25 -0300

    form input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,63 @@
-// Code goes here!
+// Autobind decorator
+function autobind(_: any, _1: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+
+}
+
+
+// Model
+class ProjectInput {
+
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templateElement.content, true)
+        
+        this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure()
+
+        this.attach()
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+
+    private configure() {
+        this.element.addEventListener(
+            'submit',
+            this.submitHandler 
+            //this.submitHandler.bind(this) // need to reference this (object) in submitHandler
+        ) 
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+    }
+}
+
+const prjInput = new ProjectInput()
